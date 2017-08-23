ad613c465c7c1b6b067521d89f7009f0d1b0d5f8
Robert Tate <rob@rtate.se>
2017-08-22 20:01:54 -0400

    Create erlang.el

---
+++ .emacs.d/erlang.el
@@ -0,0 +1,12 @@
+;; Erlang Emacs Tools
+;;
+;; Ensure EMACS_ERLANG_TOOLS and EMACS_ERLANG_ROOT are both set
+
+(defun init-erlang ()
+	(setq load-path (cons (getenv "EMACS_ERLANG_TOOLS") load-path)
+		erlang-root-dir (getenv "EMACS_ERLANG_ROOT"))
+	(require 'erlang-start)
+	(setq erlang-electric-commands '()
+		auto-save-default nil))
+
+(init-erlang)
